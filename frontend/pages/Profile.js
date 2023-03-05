720444b6f6260d2bdbd7256f2ce30912ac1a977c
km6293 <alstn7616@naver.com>
2023-03-05 18:00:08 +0900

    feat(frontend): api 연동 예제

--- frontend/pages/Profile.js
+++ frontend/pages/Profile.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, TextInput, View } from 'react-native';
 
-export default function Profile() {
+const Profile = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [nickname, setNickname] = useState('');
+
+  const handlePress = async () => {
+    try {
+      const response = await fetch('http://172.30.1.49:8080/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+          nickname
+        })
+      });
+      
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Profile Page</Text>
+    <View style={{paddingTop: 42, padding: 40}}>
+      <TextInput
+        value={name}
+        onChangeText={setName}
+        placeholder="Enter name"
+      />
+      <TextInput
+        value={email}
+        onChangeText={setEmail}
+        placeholder="Enter email"
+      />
+      <TextInput
+        value={password}
+        onChangeText={setPassword}
+        placeholder="Enter password"
+      />
+      <TextInput
+        value={nickname}
+        onChangeText={setNickname}
+        placeholder="Enter nickname"
+      />
+      <Button title="Submit" onPress={handlePress} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Profile;
