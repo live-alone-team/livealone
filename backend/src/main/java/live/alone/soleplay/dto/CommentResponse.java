6233b8084a949983e623310771b5a706ad5ee015
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-03-06 22:35:30 +0900

    fix(backend): 날짜 타입 변경

--- backend/src/main/java/live/alone/soleplay/dto/CommentResponse.java
+++ backend/src/main/java/live/alone/soleplay/dto/CommentResponse.java
@@ -4,7 +4,7 @@ import live.alone.soleplay.entity.Comment;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @Setter
@@ -12,18 +12,20 @@ public class CommentResponse {
     private Long commentId;
     private Long memberId;
     private String image;
+    private String nickname;
     private String type;
     private String title;
     private String content;
-    private LocalDateTime createdDate;
+    private String createdDate;
 
     public CommentResponse(Comment comment) {
         this.commentId = comment.getId();
         this.memberId = comment.getMember().getId();
         this.image = comment.getMember().getImage();
+        this.nickname = comment.getMember().getNickname();
         this.type = comment.getType();
         this.title = comment.getTitle();
         this.content = comment.getContent();
-        this.createdDate = comment.getCreatedDate();
+        this.createdDate = comment.getCreatedDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
     }
 }
