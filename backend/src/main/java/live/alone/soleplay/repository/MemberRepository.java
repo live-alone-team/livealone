6a3e46c85c0fa6079e36748c6b75bfb9555f4129
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-03-06 22:51:38 +0900

    feat(bakcend): 회원이 작성한 게시물 불러오는 쿼리 추가

--- backend/src/main/java/live/alone/soleplay/repository/MemberRepository.java
+++ backend/src/main/java/live/alone/soleplay/repository/MemberRepository.java
@@ -1,15 +1,17 @@
 package live.alone.soleplay.repository;
 
 import live.alone.soleplay.entity.Member;
+import live.alone.soleplay.entity.Poll;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
     Optional<Member> findByEmail(String email);
 
     Optional<Member> findById(Long id);
@@ -18,4 +20,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Modifying
     @Query("update Member m set m.jwtToken=:token where m.email=:email")
     void saveToken(String token, String email);
+
+    @Query("select p " +
+            "from Member m join Poll p on m.id = p.member.id " +
+            "where p.member.id = :memberId " +
+            "order by p.createdDate desc")
+    List<Poll> findPollHistory(@Param("memberId") Long memberId);
 }
