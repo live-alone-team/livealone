e60959a194d2740147f58720d2eb49cd9b015bef
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-03-06 22:27:26 +0900

    fix(backend): 로그인, 회원가입 분리

--- backend/src/main/java/live/alone/soleplay/controller/MemberController.java
+++ backend/src/main/java/live/alone/soleplay/controller/MemberController.java
@@ -1,68 +1,55 @@
 package live.alone.soleplay.controller;
 
+import live.alone.soleplay.config.jwt.UserDetailsImpl;
 import live.alone.soleplay.dto.*;
 import live.alone.soleplay.entity.Member;
-import live.alone.soleplay.entity.enums.Role;
-import live.alone.soleplay.config.jwt.JwtTokenProvider;
 import live.alone.soleplay.exception.CustomException;
 import live.alone.soleplay.exception.ErrorCode;
-import live.alone.soleplay.repository.MemberRepository;
 import live.alone.soleplay.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
-@RestController
 @RequiredArgsConstructor
+@RestController
+@RequestMapping("/user")
 public class MemberController {
-
     private final MemberService memberService;
-    private final MemberRepository memberRepository;
-    private final JwtTokenProvider jwtTokenProvider;
 
-    @PostMapping("/register")
-    public ResponseEntity<Member> register(@RequestBody RegisterRequest registerRequest) {
-        return ResponseEntity.ok(memberService.register(registerRequest));
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<LogInResponse> login(@RequestBody LogInRequest logInRequest) {
-        Member member = memberService.logIn(logInRequest);
-        String email = member.getEmail();
-        Role role = member.getRole();
-        String jwtToken = jwtTokenProvider.createToken(email, role);
-        memberRepository.saveToken(jwtToken, email);
-
-        return ResponseEntity.ok(new LogInResponse(email, jwtToken));
-    }
-
-    @GetMapping("/user/profile/{id}")
+    @GetMapping("/profile")
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<MemberProfile> getProfile(@PathVariable Long id) {
-        return ResponseEntity.ok(memberService.getProfile(id));
+    public ResponseEntity<MemberProfile> getProfile(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Long memberId = userDetails.getMember().getId();
+        return ResponseEntity.ok(memberService.getProfile(memberId));
     }
 
-    @GetMapping("/oauth2/success")
-    public ResponseEntity<LogInResponse> loginSuccess(@RequestParam String jwtToken, @RequestParam String email) {
-        memberRepository.saveToken(jwtToken, email);
-
-        return ResponseEntity.ok(new LogInResponse(email, jwtToken));
+    @PatchMapping("/profile")
+    public ResponseEntity<Member> updateProfile(@RequestBody MemberUpdateRequest memberUpdateRequest,
+                                                @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Long memberId = userDetails.getMember().getId();
+        return ResponseEntity.ok(memberService.updateProfile(memberUpdateRequest, memberId));
     }
 
-    @PatchMapping("/user/member/{id}")
-    public ResponseEntity<Member> updateMemberInfo(@RequestBody MemberUpdateRequest memberUpdateRequest,
-                                                   @PathVariable Long id) {
-        return ResponseEntity.ok(memberService.updateMemberInfo(memberUpdateRequest, id));
+    @GetMapping("/profile/polls")
+    public ResponseEntity<List<PollHistoryResponse>> findAllPollsBy(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Long memberId = userDetails.getMember().getId();
+        return ResponseEntity.ok(memberService.findPollHistory(memberId));
     }
 
-    @PostMapping("/user/image/{id}")
-    public ResponseEntity<Member> uploadProfile(@RequestPart MultipartFile file, @PathVariable Long id) {
-        String fileDir = "C:\\Users\\ab338\\Desktop\\soleplay\\soleplay\\src\\main\\resources\\static\\profile\\";
+    @PostMapping("/profile/image")
+    public ResponseEntity<Member> uploadProfile(@RequestPart MultipartFile file,
+                                                @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Long memberId = userDetails.getMember().getId();
+
+        // 경로를 어떻게 할지 고민 좀 하겠습니다.
+        String fileDir = "C:\\Users\\ab338\\Desktop\\live-alone\\livealone\\backend\\src\\main\\resources\\static\\profile\\";
         String original = file.getOriginalFilename();
         String savedPath = fileDir + original;
 
@@ -72,7 +59,15 @@ public class MemberController {
             throw new CustomException(ErrorCode.NOT_FOUND_IMAGE);
         }
 
-        return ResponseEntity.ok(memberService.uploadProfile(original, id));
+        return ResponseEntity.ok(memberService.uploadProfile(original, memberId));
     }
 
+    @DeleteMapping("/withdrawal")
+    public ResponseEntity<String> withdrawal(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Long memberId = userDetails.getMember().getId();
+        memberService.withdrawal(memberId);
+        String withdrawalMessage = "회원번호: " + memberId + "번\n이름: " + userDetails.getMember().getName()
+                + "\n탈퇴하였습니다.";
+        return ResponseEntity.ok(withdrawalMessage);
+    }
 }
