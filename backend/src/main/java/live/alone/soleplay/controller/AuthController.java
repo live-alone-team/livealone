45b1060ac8c1d71b463fdba48dbe7948376692d3
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-03-06 22:26:44 +0900

    fix(backend): 로그인, 회원가입으로 파일 분리

---
+++ backend/src/main/java/live/alone/soleplay/controller/AuthController.java
@@ -0,0 +1,49 @@
+package live.alone.soleplay.controller;
+
+import live.alone.soleplay.config.jwt.JwtTokenProvider;
+import live.alone.soleplay.dto.LogInRequest;
+import live.alone.soleplay.dto.LogInResponse;
+import live.alone.soleplay.dto.RegisterRequest;
+import live.alone.soleplay.entity.Member;
+import live.alone.soleplay.entity.enums.Role;
+import live.alone.soleplay.repository.MemberRepository;
+import live.alone.soleplay.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class AuthController {
+    private final MemberService memberService;
+    private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @GetMapping("/api/test")
+    public String hello() {
+        return "HELLO WORLD";
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Member> register(@RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(memberService.register(registerRequest));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LogInResponse> login(@RequestBody LogInRequest logInRequest) {
+        Member member = memberService.logIn(logInRequest);
+        String email = member.getEmail();
+        Role role = member.getRole();
+        String jwtToken = jwtTokenProvider.createToken(email, role);
+        memberRepository.saveToken(jwtToken, email);
+
+        return ResponseEntity.ok(new LogInResponse(email, jwtToken));
+    }
+
+    @GetMapping("/oauth2/success")
+    public ResponseEntity<LogInResponse> loginSuccess(@RequestParam String jwtToken, @RequestParam String email) {
+        memberRepository.saveToken(jwtToken, email);
+
+        return ResponseEntity.ok(new LogInResponse(email, jwtToken));
+    }
+}
