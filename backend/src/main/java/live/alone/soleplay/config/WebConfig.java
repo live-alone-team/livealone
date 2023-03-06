46d9004af0470945578e0024b192d766e33de611
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-03-06 22:25:59 +0900

    feat(backend): cors 맵핑 추가

--- backend/src/main/java/live/alone/soleplay/config/WebConfig.java
+++ backend/src/main/java/live/alone/soleplay/config/WebConfig.java
@@ -1,6 +1,8 @@
 package live.alone.soleplay.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,10 +10,17 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     //private String connectPath = "/test/image";
     //private String resourcePath = "file:///C:\\Users\\ab338\\Pictures\\livealone\\profile";
     final Path ROOT = Paths.get("./static/profile").toAbsolutePath().normalize();
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE").allowedOrigins("*");
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
