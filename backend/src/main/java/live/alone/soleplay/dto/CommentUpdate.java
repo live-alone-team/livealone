16b0b421fe8e72cf91c6b77f45d53a48183714ba
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-03-06 22:37:47 +0900

    fix(backend): 생성자 추가

--- backend/src/main/java/live/alone/soleplay/dto/CommentUpdate.java
+++ backend/src/main/java/live/alone/soleplay/dto/CommentUpdate.java
@@ -1,8 +1,10 @@
 package live.alone.soleplay.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class CommentUpdate {
     private String content;
 }
