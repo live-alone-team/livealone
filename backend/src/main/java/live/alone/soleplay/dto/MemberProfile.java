e960691adef5daca0e2e477a633905acf96f7f58
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-03-06 22:38:14 +0900

    fix(backend): setter 삭제

--- backend/src/main/java/live/alone/soleplay/dto/MemberProfile.java
+++ backend/src/main/java/live/alone/soleplay/dto/MemberProfile.java
@@ -2,10 +2,8 @@ package live.alone.soleplay.dto;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Builder
 public class MemberProfile {
     private String name;
