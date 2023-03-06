21fef122f15c0b6ce94681b3dc9a8a6c72d72f86
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-03-06 22:40:52 +0900

    feat(backend): 댓글 쿼리 추가

--- backend/src/main/java/live/alone/soleplay/repository/CommentRepository.java
+++ backend/src/main/java/live/alone/soleplay/repository/CommentRepository.java
@@ -2,11 +2,24 @@ package live.alone.soleplay.repository;
 
 import live.alone.soleplay.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    Optional<Comment> findById(Long id);
+
+    @Query("select c from Comment c where c.id=:commentId and c.member.id=:memberId")
+    Optional<Comment> findBy(@Param("commentId") Long commentId, @Param("memberId") Long memberId);
+
+    @Query("select c from Comment c where c.type = :type and c.title = :keyword ORDER BY c.createdDate desc")
+    List<Comment> findAll(@Param("type") String type, @Param("keyword") String keyword);
+
+    @Modifying
+    @Query("DELETE from Comment c where c.id = :commentId and c.member.id = :memberId")
+    void deleteBy(@Param("commentId") Long commentId, @Param("memberId") Long memberId);
 }
