46e041219645f7deed9313b860954c04fb00f78c
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-03-06 22:17:16 +0900

    chore(backend): 불필요한 파일 삭제

--- backend/src/main/java/live/alone/soleplay/repository/RecommendationRepository.java
+++
@@ -1,9 +0,0 @@
-package live.alone.soleplay.repository;
-
-import live.alone.soleplay.entity.Recommendation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
-}
