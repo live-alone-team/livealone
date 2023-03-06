69e60d8691f8f5cbc244c8fc6274082f2a7b8c2a
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-03-06 22:25:12 +0900

    fix(backend): 세 개의 함수를 하나의 함수로 압축

--- backend/src/main/java/live/alone/soleplay/controller/ApiController.java
+++ backend/src/main/java/live/alone/soleplay/controller/ApiController.java
@@ -1,73 +1,51 @@
 package live.alone.soleplay.controller;
 
 import live.alone.soleplay.config.MovieApiClient;
+import live.alone.soleplay.config.TvApiClient;
 import live.alone.soleplay.config.YouTubeApiClient;
-import live.alone.soleplay.dto.*;
-import live.alone.soleplay.service.CommentService;
+import live.alone.soleplay.dto.openapi.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Arrays;
 
 @RequiredArgsConstructor
 @RequestMapping("/user")
 @RestController
 public class ApiController {
     private final MovieApiClient movieApiClient;
+    private final TvApiClient tvApiClient;
     private final YouTubeApiClient youTubeApiClient;
-    private final CommentService commentService;
 
-    @GetMapping("/chart/movies")
-    public ResponseEntity<TrendingMovieResponse> getTrendingMovies() {
-        return ResponseEntity.ok(movieApiClient.getTrendingMovies());
+    @GetMapping("/chart/{type}")
+    public ResponseEntity<?> getTrending(@PathVariable String type) {
+        if (type.equals("movies"))
+            return ResponseEntity.ok(movieApiClient.getTrendingMovies());
+        else if (type.equals("tv"))
+            return ResponseEntity.ok(tvApiClient.getTrendingTvShows());
+        else
+            return ResponseEntity.ok(youTubeApiClient.getPopularVideosList());
     }
 
-    @GetMapping("/chart/tv")
-    public ResponseEntity<TrendingTvResponse> getTrendingTvShows() {
-        return ResponseEntity.ok(movieApiClient.getTrendingTvShows());
-    }
-
-    @GetMapping("/chart/youtube")
-    public ResponseEntity<YoutubeResponse> getYouTubeList() {
-        return ResponseEntity.ok(youTubeApiClient.getPopularVideosList());
-    }
-
-    @GetMapping("/search/movies/{keyword}")
-    public ResponseEntity<SearchMovieRequest> searchMovie(@PathVariable String keyword) {
-        return ResponseEntity.ok(movieApiClient.searchMovies(keyword));
-    }
-
-    @GetMapping("/search/youtube/{keyword}")
-    public ResponseEntity<YoutubeSearchResponse> searchOnYoutube(@PathVariable String keyword) {
-        return ResponseEntity.ok(youTubeApiClient.searchOnYoutube(keyword));
-    }
-
-    @GetMapping("/search/all/{type}/{keyword}")
-    public ResponseEntity<?> searchAll(@PathVariable String type, @PathVariable String keyword) {
-        if (type.equals("movie"))
+    @GetMapping("/search/{type}/{keyword}")
+    public ResponseEntity<?> search(@PathVariable String type, @PathVariable String keyword) {
+        if (type.equals("movies"))
             return ResponseEntity.ok(movieApiClient.searchMovies(keyword));
         else if (type.equals("tv"))
-            return ResponseEntity.ok(movieApiClient.searchTv(keyword));
+            return ResponseEntity.ok(tvApiClient.searchTv(keyword));
         else
             return ResponseEntity.ok(youTubeApiClient.searchOnYoutube(keyword));
     }
 
-    @PostMapping("/chart/{type}/{keyword}/comment/{memberId}")
-    public ResponseEntity<CommentRequest> saveComment(@RequestBody CommentRequest commentRequest
-            , @PathVariable String type, @PathVariable String keyword, @PathVariable Long memberId) {
-        return ResponseEntity.ok(commentService.saveComment(commentRequest, type, keyword, memberId));
+    /*public String getDetailTV(String title) {
+        tvApiClient.searchTv(title);
+
     }
 
-    @GetMapping("/chart/{type}/{keyword}/comments")
-    public ResponseEntity<List<CommentResponse>> findAllComments(@PathVariable String type, @PathVariable String keyword) {
-        return ResponseEntity.ok(commentService.findAllComments());
-    }
-
-    @DeleteMapping("/comment/{commentId}")
-    public ResponseEntity<String> deleteComment(@PathVariable Long commentId) {
-        commentService.deleteComment(commentId);
-        return ResponseEntity.ok(commentId + "번 댓글이 삭제되었습니다.");
-    }
+    public void getId() {
+        TvSearchResponse tvSearchResponse = new TvSearchResponse();
+        System.out.println(tvSearchResponse.getResults());
+    }*/
 
 }
