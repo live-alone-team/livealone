fd2b2a0f6b6c718d06fd17efcefa3f99de92b30d
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-03-06 23:23:47 +0900

    feat(backend): 회원 투표 내역 dto 추가

---
+++ backend/src/main/java/live/alone/soleplay/dto/PollHistoryResponse.java
@@ -0,0 +1,19 @@
+package live.alone.soleplay.dto;
+
+import live.alone.soleplay.entity.Poll;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.format.DateTimeFormatter;
+
+@Getter
+public class PollHistoryResponse {
+    private String title;
+    private String createdDate;
+
+    public PollHistoryResponse(Poll poll) {
+        this.title = poll.getTitle();
+        this.createdDate = poll.getCreatedDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
+
+}
