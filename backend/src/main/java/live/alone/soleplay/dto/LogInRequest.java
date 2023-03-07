69f5cfbfc18d731d7a2ddc7e1b510c7cb99fe10f
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-03-07 15:19:14 +0900

    fix(backend): 생성자 추가

--- backend/src/main/java/live/alone/soleplay/dto/LogInRequest.java
+++ backend/src/main/java/live/alone/soleplay/dto/LogInRequest.java
@@ -2,10 +2,12 @@ package live.alone.soleplay.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class LogInRequest {
     private String email;
