e0f5c79d0bfb6f4697cc6c3164b23585963e4c67
km6293 <alstn7616@naver.com>
2023-03-10 01:41:48 +0900

    feat(frontend): 배너, youtube 기능 추가

--- frontend/pages/Recommend.js
+++ frontend/pages/Recommend.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
-import { WebView } from 'react-native-webview';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput, ScrollView} from 'react-native';
 import Swiper from 'react-native-swiper';
+import {IP} from '@env';
 
 const Recommend = () => {
-  const [liked, setLiked] = useState(false);
+  const [search, setSearch] = useState(false);
+  const [visible, setVisible] = useState(false);
+
   const [state, setState] = useState({ 
     movie: { title: [], image: [], num: [] },
     tv: { title: [], image: [], num: [] },
+    youtube: {title: [], image: [], num: []}
   });
 
   const handlePress = async (url, dataKey) => {
@@ -22,9 +25,20 @@ const Recommend = () => {
       })
       const data = await response.json();
       let [title, image, num] = [[],[],[]];
-      data.results.map((item,i) => {
-        title.push(item[dataKey === "tv" ? "name" : "title"]);
-        image.push(`https://image.tmdb.org/t/p/w500${item.poster_path}`);
+
+      // youtube만 구조가 조금 다름
+      let results = dataKey === 'youtube' ? data.items : data.results;
+      results.map((item,i) => {
+        if(dataKey === 'movie'){
+          title.push(item["title"])
+          image.push(`https://image.tmdb.org/t/p/w500${item.poster_path}`);
+        }else if(dataKey === 'tv'){
+          title.push(item["name"])
+          image.push(`https://image.tmdb.org/t/p/w500${item.poster_path}`);
+        }else{
+          title.push(item.snippet.title)
+          image.push(`${item.snippet.thumbnails.standard.url}`);
+        }
         num.push(i);
       });
       setState(prevState => ({ 
@@ -37,8 +51,9 @@ const Recommend = () => {
   };
 
   useEffect(() => {
-    handlePress('http://192.168.0.122:8080/user/chart/movies', 'movie');
-    handlePress('http://192.168.0.122:8080/user/chart/tv', 'tv');
+    handlePress(`http://${IP}:8080/user/chart/movies`, 'movie');
+    handlePress(`http://${IP}:8080/user/chart/tv`, 'tv');
+    handlePress(`http://${IP}:8080/user/chart/youtube`, 'youtube');
   }, []);
 
 
@@ -49,7 +64,6 @@ const Recommend = () => {
         <View style={{height: 190}}>
           <Swiper
             showsPagination={false}
-            showsButtons={true}
             loop={true}
             autoplay={true}
             autoplayTimeout={2.5}
@@ -83,29 +97,75 @@ const Recommend = () => {
     <View style={styles.container}>
       <View style={styles.header} />
 
-      {/* 추천 , 검색 */}
       <View style={{backgroundColor:'#FFFFFF'}}>
+        
+        {/* 타이틀 , 검색 */}
         <View>
           <View style={styles.topTitle}>
-            <Text style={{fontWeight: 'bold', fontSize: 18}}>추천</Text>
-            <TouchableOpacity onPress={() => setLiked(!liked)}>
+            
+            {search ? (
+                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                  <TouchableOpacity onPress={() => setSearch(!search)}>
+                    <Image source={require('./../assets/images/arrow-right.png')}/>
+                  </TouchableOpacity>
+                  <View style={styles.searchBox}>
+                    <TouchableOpacity onPress={() => setVisible(!visible)}>
+                      <Image style={styles.searchGubun} source={require('./../assets/images/arrow-down.png')}/>
+                    </TouchableOpacity>
+
+
+                    <View style={{ borderWidth: 0.5, height: 28, borderColor: '#D0D5DA' }} />
+                    <TextInput style={styles.searchBar} placeholder="검색어를 입력하세요" />
+                  </View>
+                </View>
+              ) : (
+                <Text style={{fontWeight: 'bold', fontSize: 18}}>나혼자산다</Text>
+              )}
+
+            <TouchableOpacity onPress={() => setSearch(!search)}>
               <Image source={require('./../assets/images/search-icon.png')} />
             </TouchableOpacity>
           </View>
         </View>
-        {/* 영화 */}
-        {renderMedia("주간 인기 영화", state.movie, "movie")}
-    
-        <View style={{ height: 16, backgroundColor:'#EEEEEE' }} />
 
-        {/* 드라마 */}
-        {renderMedia("주간 인기 드라마", state.tv, "tv")}
-        
+        <ScrollView>
+          {/* 배너 */}
+          <View style={styles.banner}>
+            <Swiper
+              showsPagination={false}
+              showsButtons={true}
+              loop={true}
+              autoplay={true}
+              autoplayTimeout={2.5}
+              buttonWrapperStyle={{paddingHorizontal: 15}}
+              >
+              <Image style={styles.searchGubun} source={require('./../assets/images/banner1.png')}/>
+              <Image style={styles.searchGubun} source={require('./../assets/images/banner2.png')}/>
+            </Swiper>
+          </View>
+
+          {/* movie */}
+          {renderMedia("주간 인기 영화", state.movie, "movie")}
+      
+          <View style={{ height: 16, backgroundColor:'#EEEEEE' }} />
+
+          {/* tv */}
+          {renderMedia("주간 인기 드라마", state.tv, "tv")}
+
+          <View style={{ height: 16, backgroundColor:'#EEEEEE' }} />
+
+          {/* youtube */}
+          {renderMedia("YoutTube", state.youtube, "youtube")}
+
+          <View style={{ height: 110}} />
+
+        </ScrollView>
       </View>
     </View>
   );
 }
 
+// css
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -118,18 +178,22 @@ const styles = StyleSheet.create({
     flex:1,
   },
   subTitle: {
-    fontWeight: 'bold', 
+    fontWeight: '500',
     fontSize: 18, 
     paddingLeft: 20, 
     paddingBottom: 13, 
-    paddingTop:20
+    paddingTop:20,
   },
   topTitle: {
     width: '100%',
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    padding: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 20,
+    height: 40,
+    marginBottom:20
   },
   slideContainer: {
     flex: 1,
@@ -146,8 +210,29 @@ const styles = StyleSheet.create({
   contentTitle: {
     marginTop: 7,
     fontSize: 14,
-    textAlign: 'center',
+    fontWeight: '500'
   },
+  searchBox:{
+    borderWidth: 0.5,
+    borderRadius: 5,
+    borderColor: '#D0D5DA',
+    flexDirection: 'row', 
+    alignItems: 'center',
+    marginLeft: 20,
+  },
+  searchBar:{
+    width: 206,
+    height: 40,
+    marginLeft: 15,
+  },
+  searchGubun:{
+    marginLeft: 15,
+    marginRight: 15
+  },
+  banner:{
+    height: 170,
+    resizeMode:"stretch"
+  }
 })
 
 
