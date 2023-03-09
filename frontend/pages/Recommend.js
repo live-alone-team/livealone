8d7e39d97bf69315f448499ac4362948d5c8a431
km6293 <alstn7616@naver.com>
2023-03-09 17:10:30 +0900

    fix(frontend): 추천페이지 표출 틀 진행 및 코드 정리

--- frontend/pages/Recommend.js
+++ frontend/pages/Recommend.js
@@ -6,61 +6,47 @@ import Swiper from 'react-native-swiper';
 
 const Recommend = () => {
   const [liked, setLiked] = useState(false);
-  const [state, setState] = useState({
-    movieTitle: [],
-    movieImage: [],
-    movieNum:[]
+  const [state, setState] = useState({ 
+    movie: { title: [], image: [], num: [] },
+    tv: { title: [], image: [], num: [] },
   });
 
-  const handlePress = async () => {
+  const handlePress = async (url, dataKey) => {
     try {
-      const response = await fetch('http://192.168.0.33:8080/user/chart/movies', {
+      const response = await fetch(url, {
         method: 'GET',
         headers: {
           "Content-Type": "application/json",
-          "X-AUTH-TOKEN": "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsaXZlQWxvbmVAZ21haWwuY29tIiwicm9sZXMiOiJST0xFX1VTRVIiLCJpYXQiOjE2NzgyNjQ1ODEsImV4cCI6MTY3ODM1MDk4MX0.DX75FlN9VhdxjqJ409eKJxlhA80iE9uzx37LxGp2D0c"
+          "X-AUTH-TOKEN": "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsaXZlQWxvbmVAZ21haWwuY29tIiwicm9sZXMiOiJST0xFX1VTRVIiLCJpYXQiOjE2NzgzMzk1NTIsImV4cCI6MTY3ODQyNTk1Mn0.8MEpJUHTSLVKumdbYdx9P2bRT27asjSrtXSaBuQvqmA"
         },
       })
-      .then((response) => response.json())
-      .then((data) => {
-        let [movieTitle, movieImage, movieNum] = [[],[],[]];
-        data.results.map((movie,i) => {
-          movieTitle.push(movie.title);
-          movieImage.push(`https://image.tmdb.org/t/p/w500${movie.poster_path}`);
-          movieNum.push(i);
-        })
-        setState({ 
-          movieTitle: state.movieTitle,
-          movieImage: state.movieImage,
-          movieNum: state.movieNum
-        });
-      }) 
+      const data = await response.json();
+      let [title, image, num] = [[],[],[]];
+      data.results.map((item,i) => {
+        title.push(item[dataKey === "tv" ? "name" : "title"]);
+        image.push(`https://image.tmdb.org/t/p/w500${item.poster_path}`);
+        num.push(i);
+      });
+      setState(prevState => ({ 
+        ...prevState,
+        [dataKey]: {title, image, num}
+      }));
     } catch (error) {
       console.error(error);
     }
   };
 
   useEffect(() => {
-    handlePress(); 
+    handlePress('http://192.168.0.122:8080/user/chart/movies', 'movie');
+    handlePress('http://192.168.0.122:8080/user/chart/tv', 'tv');
   }, []);
 
-  return (
-    
-    <View style={styles.container}>
-      {/* 추천 , 검색 */}
+
+  const renderMedia = (title, data, dataKey) => {
+    return (
       <View>
-        <View style={styles.header} />
-        <View style={styles.topTitle}>
-          <Text style={{fontWeight: 'bold', fontSize: 18}}>추천</Text>
-          <TouchableOpacity onPress={() => setLiked(!liked)}>
-            <Image source={require('./../assets/images/search-icon.png')} />
-          </TouchableOpacity>
-        </View>
-      </View>
-      {/* 유튜브 추천 리뷰 */}
-      <View>
-        <Text style={{fontWeight: 'bold', fontSize: 18, paddingLeft: 20, paddingBottom: 13}}>유튜브 추천 리뷰</Text>
-        <View style={{backgroundColor: 'black', height: 161}}>
+        <Text style={styles.subTitle}>{title}</Text>
+        <View style={{height: 190}}>
           <Swiper
             showsPagination={false}
             showsButtons={true}
@@ -69,35 +55,52 @@ const Recommend = () => {
             autoplayTimeout={2.5}
             buttonWrapperStyle={{paddingHorizontal: 15}}
           >
-            {/* 1 ~ 20 => 1 2 3  4 5 6 */}
-            {state.movieImage.map((e,i) => (
-              (i+1) <= 12  
-                ? (i+1) % 3 == 1 
-                    ? <View style={styles.slideContainer}> 
-                      <Image
-                        style={ styles.slide }
-                        source={{ uri: state.movieImage[i] }}
-                      /> 
-                      <View style={{width: '5%'}} />
-                    : null
-                  (i+1) % 3 == 2 
-                    ? <Image
-                        style={ styles.slide }
-                        source={{ uri: state.movieImage[i] }}
-                      /> 
-                      <View style={{width: '5%'}} />
-                    : null
-                  (i+1) % 3 == 0 
-                    ? <Image
-                        style={ styles.slide }
-                        source={{ uri: state.movieImage[i] }}
-                      />
-                      </View> 
-                    : null        
-                : null
-            ))}
+
+          {/* 임시로 12개만 돌림 */}
+          {[0, 3, 6, 9].map((startIndex, swiperIndex) => (
+            <View key={swiperIndex} style={{ flexDirection: "row" }}>
+              {[startIndex, startIndex + 1, startIndex + 2].map((index) => (
+                <React.Fragment key={index}>
+                  <View style={styles.slideContainer}>
+                    <Image style={styles.slide} source={{ uri: data.image[index] }} />
+                    <Text style={styles.contentTitle}>{data.title[index]}</Text>
+                    {index % 3 !== 2 && (
+                      <View style={{ width: "5%" }} />
+                    )}
+                  </View>
+                </React.Fragment>
+              ))}
+            </View>
+          ))}
           </Swiper>
         </View>
+      </View>
+      );
+    };  
+
+  return (
+    
+    <View style={styles.container}>
+      <View style={styles.header} />
+
+      {/* 추천 , 검색 */}
+      <View style={{backgroundColor:'#FFFFFF'}}>
+        <View>
+          <View style={styles.topTitle}>
+            <Text style={{fontWeight: 'bold', fontSize: 18}}>추천</Text>
+            <TouchableOpacity onPress={() => setLiked(!liked)}>
+              <Image source={require('./../assets/images/search-icon.png')} />
+            </TouchableOpacity>
+          </View>
+        </View>
+        {/* 영화 */}
+        {renderMedia("주간 인기 영화", state.movie, "movie")}
+    
+        <View style={{ height: 16, backgroundColor:'#EEEEEE' }} />
+
+        {/* 드라마 */}
+        {renderMedia("주간 인기 드라마", state.tv, "tv")}
+        
       </View>
     </View>
   );
@@ -106,12 +109,20 @@ const Recommend = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#EEEEEE'
   },
   header: {
-    height:60,
+    height:50,
   },
   content: {
     flex:1,
+  },
+  subTitle: {
+    fontWeight: 'bold', 
+    fontSize: 18, 
+    paddingLeft: 20, 
+    paddingBottom: 13, 
+    paddingTop:20
   },
   topTitle: {
     width: '100%',
@@ -122,18 +133,22 @@ const styles = StyleSheet.create({
   },
   slideContainer: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'column',
     justifyContent: 'space-between',
     alignItems: 'center',
-    // justifyContent: 'flex-start',
     paddingHorizontal: 10,
   },
   slide: {
     width: 99,
     height: 138,
-    backgroundColor: 'gray',
     borderRadius: 5,
   },
+  contentTitle: {
+    marginTop: 7,
+    fontSize: 14,
+    textAlign: 'center',
+  },
 })
+
 
 export default Recommend;
