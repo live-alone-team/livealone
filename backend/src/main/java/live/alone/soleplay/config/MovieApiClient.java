bbd876465495904c8238ff2875e9e8e2f19d7eb5
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-03-10 21:29:20 +0900

    feat(backend): 영화 상세보기 추가

--- backend/src/main/java/live/alone/soleplay/config/MovieApiClient.java
+++ backend/src/main/java/live/alone/soleplay/config/MovieApiClient.java
@@ -1,5 +1,6 @@
 package live.alone.soleplay.config;
 
+import live.alone.soleplay.dto.openapi.MovieDetailResponse;
 import live.alone.soleplay.dto.openapi.MovieSearchResponse;
 import live.alone.soleplay.dto.openapi.TrendingMovieResponse;
 
@@ -19,6 +20,7 @@ public class MovieApiClient {
     private final String TMDB_KEY = "3201b7c0835e166642b793c241b359e4";
     private final String trendingMovieUrl = "https://api.themoviedb.org/3/trending/movie/week?region=ko-KR&language=ko-KR&api_key=" + TMDB_KEY;
     private final String movieSearchUrl = "https://api.themoviedb.org/3/search/movie?region=ko-KR&language=ko-KR&query={keyword}&api_key=" + TMDB_KEY;
+    private final String movieDetailsUrl = "https://api.themoviedb.org/3/movie/{id}?language=ko-KR&api_key=" + TMDB_KEY;
 
     public TrendingMovieResponse getTrendingMovies() {
         final HttpHeaders httpHeaders = new HttpHeaders();
@@ -35,6 +37,15 @@ public class MovieApiClient {
 
         return restTemplate
                 .exchange(movieSearchUrl, HttpMethod.GET, entity, MovieSearchResponse.class, keyword)
+                .getBody();
+    }
+
+    public MovieDetailResponse getMovieDetails(Long id) {
+        final HttpHeaders httpHeaders = new HttpHeaders();
+        final HttpEntity<String> entity = new HttpEntity<>(httpHeaders);
+
+        return restTemplate
+                .exchange(movieDetailsUrl, HttpMethod.GET, entity, MovieDetailResponse.class, id)
                 .getBody();
     }
 
