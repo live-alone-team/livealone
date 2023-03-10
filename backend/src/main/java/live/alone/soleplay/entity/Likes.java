518054700846461d83bab75ef8623fd45de9f32b
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-03-10 22:45:52 +0900

    feat(backend): 좋아요한 시간 추가

--- backend/src/main/java/live/alone/soleplay/entity/Likes.java
+++ backend/src/main/java/live/alone/soleplay/entity/Likes.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Likes {
+public class Likes extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "like_id")
