46252b82b42792bde4ef69635e939cb328ee947c
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-03-11 02:35:29 +0900

    feat(backend): 투표 리스트 조회 dto 추가

--- backend/src/main/java/live/alone/soleplay/dto/PollListResponse.java
+++ backend/src/main/java/live/alone/soleplay/dto/PollListResponse.java
@@ -1,16 +1,22 @@
 package live.alone.soleplay.dto;
 
 import live.alone.soleplay.entity.Poll;
+import live.alone.soleplay.util.DateMapper;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.format.DateTimeFormatter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class PollListResponse {
     private String image;
     private String nickname;
     private String title;
     private String description;
+    private String expirationDate;
     private String createdTime;
 
     public PollListResponse(Poll poll) {
@@ -18,6 +24,7 @@ public class PollListResponse {
         this.nickname = poll.getMember().getNickname();
         this.title = poll.getTitle();
         this.description = poll.getDescription();
-        this.createdTime = poll.getCreatedDate().format(DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm"));
+        this.expirationDate = DateMapper.calculateTime(poll.getExpirationDate());
+        this.createdTime = poll.getCreatedDate().format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
     }
 }
