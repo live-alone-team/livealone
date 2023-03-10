e4e182fde99251f518d271d107dec3cd42ee4097
Gahyeon-Lee <gahyeon.plum@gmail.com>
2023-03-10 22:40:28 +0900

    feat(backend): 좋아요 엔티티 추가

---
+++ backend/src/main/java/live/alone/soleplay/entity/Likes.java
@@ -0,0 +1,41 @@
+package live.alone.soleplay.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Likes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id")
+    private Long id;
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "poll_id", nullable = false)
+    private Poll poll;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column
+    private int status;
+
+    public Likes(Poll poll, Member member, int status) {
+        this.poll = poll;
+        this.member = member;
+        this.status = status;
+    }
+}
