3a1ecaa18608605190f0e5d9b8ac75b227482b94
km6293 <alstn7616@naver.com>
2023-03-13 02:35:33 +0900

    chore(frontend): v4 -> v5 코드 수정

--- frontend/App.js
+++ frontend/App.js
@@ -1,99 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { createBottomTabNavigator, createAppContainer} from 'react-navigation';  
-import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';  
-// import Icon from 'react-native-vector-icons/Ionicons'; 
+import { View, Image } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import Vote from './pages/Vote';
 import MainPage from './pages/MainPage';
 import Recommend from './pages/Recommend';
 import Profile from './pages/Profile';
-import axios from 'axios';
 
-// axios.get('http://localhost:8080/api/users')
-//   .then(response => {
-//     console.log(response);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
+const Tab = createBottomTabNavigator();
 
-function App() {
+const TabNavigator = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen
+        name="MainPage"
+        component={MainPage}
+        options={{
+          tabBarLabel: '메인 화면',
+          tabBarIcon: ({ color, size }) => (
+            <View>
+              <Image source={require('./assets/images/home-icon.png')} />
+            </View>
+          ),
+          headerShown: false
+        }}
+      />
+      <Tab.Screen
+        name="Vote"
+        component={Vote}
+        options={{
+          tabBarLabel: '투표 추가',
+          tabBarIcon: ({ color, size }) => (
+            <View>
+              <Image source={require('./assets/images/vote-icon.png')} />
+            </View>
+          ),
+          headerShown: false
+        }}
+      />
+      <Tab.Screen
+        name="Recommend"
+        component={Recommend}
+        options={{
+          tabBarLabel: '투표',
+          tabBarIcon: ({ color, size }) => (
+            <View>
+              <Image source={require('./assets/images/recommend-icon.png')} />
+            </View>
+          ),
+          headerShown: false
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: '프로필',
+          tabBarIcon: ({ color, size }) => (
+            <View>
+              <Image source={require('./assets/images/profile-icon.png')} />
+            </View>
+          ),
+          headerShown: false
+        }}
+      />
+    </Tab.Navigator>
   );
-}
+};
 
-const styles = StyleSheet.create({  
-  container: {  
-      flex: 1,  
-      justifyContent: 'center',  
-      alignItems: 'center'  
-  },  
-});
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <TabNavigator />
+    </NavigationContainer>
+  );
+};
 
-const TabNavigator = createMaterialBottomTabNavigator(  
-  { 
-      MainPage: { 
-        screen: MainPage,  
-        navigationOptions:{  
-          tabBarLabel:'메인 화면',  
-          tabBarIcon: ({  }) => (  
-              <View>
-                  {/* <Icon style={[{color: tintColor}]} size={25} name={'home-outline'}/>   */}
-                  <Image source={require('./assets/images/home-icon.png')} />
-              </View>),  
-        }  
-      },  
-      Vote: { 
-        screen: Vote,  
-        navigationOptions:{  
-          tabBarLabel:'투표 추가',  
-          tabBarIcon: ({  }) => (  
-            <View>  
-                  {/* <Icon style={[{color: tintColor}]} size={25} name={'add-circle-outline'}/>   */}
-                  <Image source={require('./assets/images/vote-icon.png')} />
-              </View>),  
-          activeColor:   '#6B7583',  
-          inactiveColor: '#6B7583',  
-          barStyle: { backgroundColor: 'white', paddingBottom: 30 },  
-        }  
-      },  
-      Recommend: { 
-        screen: Recommend,  
-        navigationOptions:{  
-          tabBarLabel:'투표',  
-          tabBarIcon: ({  }) => (  
-            <View>  
-                  {/* <Icon style={[{color: tintColor}]} size={25} name={'chatbubbles-outline'}/>   */}
-                  <Image source={require('./assets/images/recommend-icon.png')} />
-              </View>),  
-          activeColor:   '#6B7583',  
-          inactiveColor: '#6B7583',  
-          barStyle: { backgroundColor: 'white', paddingBottom: 30 },  
-        }  
-      },  
-      Profile: {  
-        screen: Profile,  
-        navigationOptions:{  
-          tabBarLabel:'프로필',  
-          tabBarIcon: ({  }) => (  
-            <View>  
-                  {/* <Icon style={[{color: tintColor}]} size={25} name={'person-outline'}/>   */}
-                  <Image source={require('./assets/images/profile-icon.png')} />
-              </View>),  
-        }  
-      },  
-    },  
-  {  
-    initialRouteName: "MainPage",  
-    activeColor:   '#6B7583',  
-    inactiveColor: '#6B7583',  
-    barStyle: { backgroundColor: 'white', paddingBottom: 30 },  
-  },  
-);  
-
-export default createAppContainer(TabNavigator)
+export default AppNavigator;
