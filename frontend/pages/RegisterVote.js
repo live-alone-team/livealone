68f179023d344cf1b3156536de736af77edef678
km6293 <alstn7616@naver.com>
2023-04-05 06:23:30 +0900

    feat(frontend): 투표 등록 페이지 등록하기 완료

--- frontend/pages/RegisterVote.js
+++ frontend/pages/RegisterVote.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, ScrollView, SafeAreaView, StatusBar, Text, TouchableOpacity, TextInput } from 'react-native';
+import { StyleSheet, View, ScrollView, SafeAreaView, StatusBar, Text, TouchableOpacity, TextInput, Alert } from 'react-native';
 import { IP, TOKEN } from '@env';
 import SelectDropdown from 'react-native-select-dropdown'
-import { useNavigation , CommonActions, useIsFocused} from '@react-navigation/native';
+import { useNavigation , CommonActions, useIsFocused, useRoute} from '@react-navigation/native';
 import { getToken  } from './token';
 import { SimpleLineIcons, Entypo, FontAwesome5 } from '@expo/vector-icons';
 
@@ -10,19 +10,16 @@ const RegisterVote = () => {
 
   const [title, setTitle] = useState(''); 
   const [content, setContent] = useState(''); 
-  const [contentsVal, setContentsVal] = useState('');
+  const [inputs, setInputs] = useState([{ key: 1, text: '' }]); // 자식 페이지에서 받아올 값
   const day = ["0","1","2","3","4","5","6","7"]
   const hour = [ "0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"]
-  const minute = [
-    "0","1","2","3","4","5","6","7","8","9",
-    "10","11","12","13","14","15","16","17","18","19",
-    "20","21","22","23","24","25","26","27","28","29",
-    "30","31","32","33","34","35","36","37","38","39",
-    "40","41","42","43","44","45","46","47","48","49",
-    "50","51","52","53","54","55","56","57","58","59",
-  ]
+  const minute = [ "0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59",]
+  const [selectedDay, setSelectedDay] = useState("0");
+  const [selectedHour, setSelectedHour] = useState("0");
+  const [selectedMinute, setSelectedMinute] = useState("0");
 
   const navigation = useNavigation();
+  const { params } = useRoute();
 
   const [token, setToken] = useState('');
   const isFocused = useIsFocused();
@@ -44,11 +41,67 @@ const RegisterVote = () => {
     }
   }, [isFocused]);
 
+  useEffect(() => {
+    if (params !== undefined && params !== inputs) {
+      setInputs(params);
+    }
+  }, [params, inputs]);
+
   const addVoteMove = () => {
-    navigation.navigate('AddVote',{
-      
-    });
+    navigation.navigate('AddVote',{});
   };
+
+  
+  const writeVote = async () => {
+    const userToken = await getToken();
+
+    if(!title) {
+      Alert.alert('제목을 입력하여주세요.', '', [{text: '확인'},])
+      return
+    }
+    if(!content || content.length < 10) {
+      Alert.alert('내용을 10자 이상 입력하여주세요.', '', [{text: '확인'},])
+      return
+    }
+    if(inputs.length == 1){
+      Alert.alert('투표를 추가하여주세요.', '', [{text: '확인'},])
+      return
+    }    
+    if(selectedDay == '0' && selectedHour == '0' && selectedMinute == '0'){
+      Alert.alert('투표기간을 선택하여주세요.', '', [{text: '확인'},])
+      return
+    }    
+    const choices = inputs.inputs.map((input) => ({ content: input.text }));
+
+    try {
+      const response = await fetch(`http://${IP}:8080/user/poll/save`, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          "X-AUTH-TOKEN": userToken
+        },
+        body: JSON.stringify({ 
+          title : title ,
+          description : content,
+          choices : choices,
+          day:selectedDay,
+          hour:selectedHour,
+          minute:selectedMinute
+        })
+      })
+      Alert.alert('투표가 추가되었습니다.', '', [{text: '확인'},])
+
+      setTitle('');
+      setContent('');
+      setSelectedDay('0');
+      setSelectedHour('0');
+      setSelectedMinute('0');
+      setInputs([{ key: 1, text: '' }]);
+    } catch (error) {
+      console.error(error);
+    }
+  }; 
+
 
   return (
     <View style={styles.container}> 
@@ -69,8 +122,8 @@ const RegisterVote = () => {
           </View>
 
           {/* 입력란 */}
-          <View style={{height: 200, width: '87%', marginBottom: 10, marginLeft: 20 }}>
-            <TextInput value={content} onChangeText={setContent} style={{ height: '100%', width: '100%' }} placeholder='10자 이상 입력해주세요.' />
+          <View style={{height: 200, width: '87%', marginBottom: 10, marginLeft: 25 }}>
+            <TextInput multiline={true}  value={content} onChangeText={setContent} style={{ height: '100%', width: '90%', marginTop:20, marginBottom:20 }} placeholder='10자 이상 입력해주세요.' />
           </View>
           
           <View style={{alignItems: 'center'}}>
@@ -110,7 +163,7 @@ const RegisterVote = () => {
                         defaultButtonText='0'
                         data={day}
                         onSelect={(selectedItem, index) => {
-                          console.log(selectedItem, index)
+                          setSelectedDay(selectedItem);
                         }}
                         buttonTextAfterSelection={(selectedItem, index) => {
                           return selectedItem
@@ -137,7 +190,7 @@ const RegisterVote = () => {
                         defaultButtonText='0'
                         data={hour}
                         onSelect={(selectedItem, index) => {
-                          console.log(selectedItem, index)
+                          setSelectedHour(selectedItem);
                         }}
                         buttonTextAfterSelection={(selectedItem, index) => {
                           return selectedItem
@@ -164,7 +217,7 @@ const RegisterVote = () => {
                         defaultButtonText='0'
                         data={minute}
                         onSelect={(selectedItem, index) => {
-                          console.log(selectedItem, index)
+                          setSelectedMinute(selectedItem);
                         }}
                         buttonTextAfterSelection={(selectedItem, index) => {
                           return selectedItem
@@ -188,7 +241,7 @@ const RegisterVote = () => {
           <View style={{alignItems: 'center', justifyContent: 'center', height:'20%'}}>
             <View style={{flex: 1, width:'90%', height:'100%'}}>
                 <View style={{width:'100%', height:'60%'}}></View>
-                <TouchableOpacity style={{width:'100%', height:'40%'}}>
+                <TouchableOpacity onPress={() => writeVote()} style={{width:'100%', height:'40%'}}>
                   <View style={{width:'100%', height:'100%', borderWidth: 1, borderColor: '#D6D6D6', borderRadius: '5', justifyContent: 'center', alignItems: 'center', backgroundColor:'#FF4545'}}>
                     <Text style={{fontSize:20, textAlign: 'center', textAlignVertical: 'center', color:'white'}}>
                       등록하기
